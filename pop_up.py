e06c4e6a362b85188e0ac2aa7953c297bff9ec85
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-02-07 12:21:42 -0300

    Update pop_up.py
    cleaned code

--- pop_up.py
+++ pop_up.py
@@ -3,30 +3,11 @@ import random
 
 turtles = []
 
-turtle1 = t.Turtle(shape = "turtle")
-turtle1.speed("fastest")
-turtle1.penup()
-turtles.append(turtle1)
-
-turtle2 = t.Turtle(shape = "turtle")
-turtle2.speed("fastest")
-turtle2.penup()
-turtles.append(turtle2)
-
-turtle3 = t.Turtle(shape = "turtle")
-turtle3.speed("fastest")
-turtle3.penup()
-turtles.append(turtle3)
-
-turtle4 = t.Turtle(shape = "turtle")
-turtle4.speed("fastest")
-turtle4.penup()
-turtles.append(turtle4)
-
-turtle5 = t.Turtle(shape = "turtle")
-turtle5.speed("fastest")
-turtle5.penup()
-turtles.append(turtle5)
+for i in range (5):
+    new_turtle = t.Turtle(shape = "turtle")
+    new_turtle.speed("fastest")
+    new_turtle.penup()
+    turtles.append(new_turtle)
 
 directions = [0, 90, 180, 270]
 t.colormode(255)
