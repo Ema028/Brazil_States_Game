ff0d9eaa2dd3992f4960a5e2794b4d66c9fb09cb
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-02-07 11:31:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from time import sleep
 from unicodedata import normalize
+from tkinter import messagebox
 import turtle
 import pandas
+import pop_up
 
 screen = turtle.Screen()
 screen.title("Estados do Brasil")
@@ -29,10 +32,27 @@ while len(guessed_states) < 26:
                 missing_states.append(state)
         new_data = pandas.DataFrame(missing_states).to_csv("estados_faltando.csv")
         break
+
+    if ascii_answer in guessed_states:
+        messagebox.showinfo("Repetido", f"Você já acertou o estado do {answer}")
+        continue
+
+    found = False
     for state in data.state:
         ascii_state = normalize('NFKD', state).encode('ASCII', 'ignore').decode('ASCII')
         if ascii_answer == ascii_state:
-            guessed_states.append(state)
+            guessed_states.append(ascii_state)
             data_row = data[data.state == state]
             turtle.goto(data_row.x.item(), data_row.y.item())
             turtle.write(state)
+            found = True
+
+    if not found:
+        messagebox.showinfo("Errado",f"{answer} não é um estado" )
+
+sleep(1)
+turtle.clear()
+turtle.goto(-(screen.window_width()/4), 0)
+turtle.write("Parabéns", font=("Arial", 58, "normal"))
+pop_up.win()
+screen.exitonclick()
